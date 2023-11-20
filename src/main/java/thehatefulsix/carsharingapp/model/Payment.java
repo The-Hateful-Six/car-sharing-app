bd94058df795e9dfa9d48047d60cb11180bb36e1
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-11-20 19:04:25 +0200

    added Rental and Payment entities and tables

---
+++ src/main/java/thehatefulsix/carsharingapp/model/Payment.java
@@ -0,0 +1,62 @@
+package thehatefulsix.carsharingapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "payments")
+@SQLDelete(sql = "UPDATE payments SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+
+    @Column(nullable = false)
+    private Long rentalId;
+
+    @URL
+    @Column(nullable = false)
+    private String sessionUrl;
+
+    @Column(nullable = false)
+    private String sessionId;
+
+    @Column(nullable = false)
+    private BigDecimal amountToPay;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    public enum Status {
+        PENDING,
+        PAID
+    }
+
+    public enum Type {
+        PAYMENT,
+        FINE
+    }
+}
---
+++ src/main/java/thehatefulsix/carsharingapp/model/Rental.java
@@ -0,0 +1,43 @@
+package thehatefulsix.carsharingapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "rentals")
+@SQLDelete(sql = "UPDATE rentals SET is_active = false WHERE id = ?")
+@Where(clause = "is_active=true")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate rentalDate;
+
+    @Column(nullable = false)
+    private LocalDate returnDate;
+
+    @Column(nullable = false)
+    private LocalDate actualReturnDate;
+
+    @Column(nullable = false)
+    private Long carId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+}
