c5b81b81c9f511a72e0edd8d733e3ee2aa497e9d
evgenkolner <evgen.kolner@gmail.com>
2023-11-20 17:51:11 +0200

    added h2-dependencies

--- src/test/java/thehatefulsix/carsharingapp/CarSharingAppApplicationTests.java
+++ src/test/java/thehatefulsix/carsharingapp/CarSharingAppApplicationTests.java
@@ -1,11 +1,14 @@
 package thehatefulsix.carsharingapp;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class CarSharingAppApplicationTests {
 
+    @Disabled
     @Test
     void contextLoads() {
     }
