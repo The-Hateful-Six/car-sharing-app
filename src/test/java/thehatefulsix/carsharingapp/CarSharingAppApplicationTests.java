77cdc6d8e2a38c7039036e2488743bcb0576e5b0
Mykola Mosii <mykolamosii@gmail.com>
2023-11-20 10:37:10 +0200

    infrastructure setup

---
+++ src/test/java/thehatefulsix/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package thehatefulsix.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
