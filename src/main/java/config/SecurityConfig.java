d7120fa64df429537a5a66112ed83503f9e0fb94
Volodymyr Skvira <deruty13061999@gmail.com>
2023-11-20 15:38:05 +0200

    minor-formatting-change

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import security.JwtAuthenticationFilter;
 
-
 @EnableMethodSecurity
 @EnableWebSecurity
 @RequiredArgsConstructor
