833f11fec1af0bac3034d3367995710ef5259ba9
Volodymyr Skvira <deruty13061999@gmail.com>
2023-11-21 16:06:26 +0200

    Implemented Auth service and controller

--- src/main/java/thehatefulsix/carsharingapp/security/AuthenticationService.java
+++ src/main/java/thehatefulsix/carsharingapp/security/AuthenticationService.java
@@ -1,4 +1,25 @@
 package thehatefulsix.carsharingapp.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import thehatefulsix.carsharingapp.dto.user.UserLoginRequestDto;
+import thehatefulsix.carsharingapp.dto.user.UserLoginResponseDto;
+
+@RequiredArgsConstructor
+@Service
 public class AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginResponseDto authenticate(UserLoginRequestDto request) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.email(), request.password())
+        );
+
+        String token = jwtUtil.generateToken(request.email());
+        return new UserLoginResponseDto(token);
+    }
 }
